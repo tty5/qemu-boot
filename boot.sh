9fd2fd80d1d1e048ac071a6884f1701e7284cd05
tty5 <26674599+tty5@users.noreply.github.com>
2020-01-19 16:46:26 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -10,4 +10,6 @@ cu -l /dev/pts/0
 qemu-system-x86_64 -enable-kvm -nographic -serial mon:pty -smp 4 -m 32000 -drive file=CentOS-7-x86_64-GenericCloud-1905.qcow2,if=virtio #-kernel bzImage -append 'root=/dev/vda console=ttyS0,115200 nokaslr'
 
 # win10
-qemu-system-x86_64 -enable-kvm -vnc :2  -smp 16 -m 32000 -usbdevice tablet -drive file=win10-disk.qcow2,if=virtio  #-cdrom win10_1909_chinese_x64.iso -drive file=virtio-win-0.1.160.iso,index=0,media=cdrom
+# guest install 
+qemu-system-x86_64 -enable-kvm -vnc :2  -smp 16 -m 32000 -vga qxl -usbdevice tablet -drive file=win10-disk.qcow2,if=virtio #-cdrom win10_1909_chinese_x64.iso -drive file=virtio-win-0.1.160.iso,index=0,media=cdrom -redir tcp:3389::3389
+

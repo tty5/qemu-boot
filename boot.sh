df5b6c3bcc6c9570307c76e9f4cc83e8df9fd4db
tty5 <26674599+tty5@users.noreply.github.com>
2020-03-28 23:50:10 +0800

    z

--- boot.sh
+++ boot.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+# win10
+# guest install spice guest tool
+qemu-system-x86_64 -enable-kvm -vnc :2  -smp 16 -m 32000 -vga qxl -usbdevice tablet -drive file=win10-disk.qcow2,if=virtio #-cdrom win10_1909_chinese_x64.iso -drive file=virtio-win-0.1.160.iso,index=0,media=cdrom -redir tcp:3389::3389
+
 # wget https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4
 # wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1905.qcow2.xz
 
@@ -9,7 +14,13 @@ cu -l /dev/pts/0
 
 qemu-system-x86_64 -enable-kvm -nographic -serial mon:pty -smp 4 -m 32000 -drive file=CentOS-7-x86_64-GenericCloud-1905.qcow2,if=virtio #-kernel bzImage -append 'root=/dev/vda console=ttyS0,115200 nokaslr'
 
-# win10
-# guest install spice guest tool
-qemu-system-x86_64 -enable-kvm -vnc :2  -smp 16 -m 32000 -vga qxl -usbdevice tablet -drive file=win10-disk.qcow2,if=virtio #-cdrom win10_1909_chinese_x64.iso -drive file=virtio-win-0.1.160.iso,index=0,media=cdrom -redir tcp:3389::3389
+gdb vmlinux -ex 'target remote :1234'
+
+KERN_CFLAGS="CFLAGS_base.o=-Og"
+
+make clean; bear make bzImage -j200 $KERN_CFLAGS
+
+make scripts_gdb
+
+apropos lx
 

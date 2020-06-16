d8ef4e8bfadb65968c809636d1e99fde81850037
tty5 <26674599+tty5@users.noreply.github.com>
2020-06-16 23:11:39 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -6,6 +6,7 @@ qemu-system-x86_64 -enable-kvm -vnc :2  -smp 16 -m 32000 -vga qxl -usbdevice tab
 # wget https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4
 # wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1905.qcow2.xz
 
+# apt install linux-image-5.4.0-33-generic libguestfs-tools -y
 # virt-customize -a CentOS-7-x86_64-GenericCloud-1905.qcow2 --root-password password:1 --uninstall cloud-init --uninstall gssproxy --uninstall selinux-policy-targeted --uninstall selinux-policy
 
 # yum install xterm
